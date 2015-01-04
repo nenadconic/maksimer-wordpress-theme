c4808e7c345778d1b3446ce3317437a701d5a753
Fredrik Rossland <fredrik.rossland@gmail.com>
2015-01-04 20:40:57 +0100

    Fjernet tekstfarge

--- innholdsbygger-knapper.php
+++ innholdsbygger-knapper.php
@@ -1,10 +1,9 @@
 <?php
 	global $innholdsbygger_teller;
 	$bakgrunn_farge = maksimer_bg_farge();
-	$tekst_farge = maksimer_tekst_farge();
 	$padding = maksimer_padding();
 ?>
-<section id="innholdsbygger-seksjon-<?php echo $innholdsbygger_teller; ?>" class="innholdsbygger-seksjon knapper<?php echo $padding; ?>" style="<?php echo $bakgrunn_farge . $tekst_farge; ?>">
+<section id="innholdsbygger-seksjon-<?php echo $innholdsbygger_teller; ?>" class="innholdsbygger-seksjon knapper<?php echo $padding; ?>" style="<?php echo $bakgrunn_farge; ?>">
 
 	<?php if ( have_rows( 'knapper' ) ) : ?>
 
