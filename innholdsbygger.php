daf7124cf7305981df8e11e87fc45d51342e10f3
Fredrik Rossland <fredrik.rossland@gmail.com>
2015-01-05 11:48:02 +0100

    Henter bare row_layout

--- innholdsbygger.php
+++ innholdsbygger.php
@@ -7,15 +7,7 @@
 
 		while ( has_sub_field( 'innholdsbygger' ) ) :
 
-			if ( 'en_kolonne' == get_row_layout() ) {
-				get_template_part( 'innholdsbygger', 'en_kolonne' );
-			} elseif ( 'to_kolonne' == get_row_layout() ) {
-				get_template_part( 'innholdsbygger', 'to_kolonne' );
-			} elseif ( 'knapper' == get_row_layout() ) {
-				get_template_part( 'innholdsbygger', 'knapper' );
-			} else {
-				return false;
-			}
+			get_template_part( 'innholdsbygger', get_row_layout() );
 
 			$innholdsbygger_teller++;
 
