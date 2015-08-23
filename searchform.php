10fa124797e74c63f7e815e2ba29343829193427
Kim Bertelsen <kimb87@gmail.com>
2015-08-23 20:17:25 +0200

    Ny kode for søkefelt

--- searchform.php
+++ searchform.php
@@ -1,5 +1,7 @@
-<form method="get" class="sok-skjema" action="<?php bloginfo( 'url' ); ?>/">
-	<label class="skjult" for="s"><?php _e( 'Søk etter', 'maksimer_lang' ); ?></label>
-	<input type="text" value="<?php the_search_query(); ?>" name="s" class="sok-felt" />
-	<button type="submit" class="sok-knapp knapp"><?php _e( 'Søk', 'maksimer_lang' ); ?></button>
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<label>
+		<span class="skjult"><?php _e( 'Søk etter:', 'maksimer_lang' ); ?></span>
+		<input type="search" class="sok-felt" placeholder="<?php _e( 'Søk etter:', 'maksimer_lang' ); ?>" value="<?php the_search_query() ?>" name="s" title="<?php _e( 'Søk etter:', 'maksimer_lang' ); ?>" />
+	</label>
+	<input type="submit" class="sok-knapp knapp" value="<?php _e( 'Søk', 'maksimer_lang' ); ?>" />
 </form>
