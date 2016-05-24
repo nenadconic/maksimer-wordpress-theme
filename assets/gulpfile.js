e40748d285f7cf19ed380bbff36f00bbed6a562d
Fredrik Rossland <fredrik.rossland@gmail.com>
2016-05-24 15:08:10 +0200

    Wrong files
    Uploaded wrong files from vagrant, these are the correct files i've been working on

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -1,24 +1,56 @@
 var gulp         = require('gulp');
+var plumber      = require('gulp-plumber');
 var sass         = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var notify       = require('gulp-notify');
+var uglify       = require('gulp-uglify');
+var concat       = require('gulp-concat');
+
+
 
 
 /*
- * Sass compile task
+ * JavaScript compile task
 */
-gulp.task('sass', function () {
+gulp.task('scripts', function() {
+  gulp.src(['js/**/*.js', '!js/analyse.js'])
+    .pipe(concat('maksimer.min.js'))
+    .pipe(gulp.dest('js/min/'))
+    .pipe(uglify())
+    .pipe(gulp.dest('js/min/'))
+});
+
+
+/*
+ * Style compile task
+*/
+gulp.task('styles', function () {
    gulp.src('sass/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe( autoprefixer( 'last 2 version', 'ie 8', 'ie 9' ) )
+    .pipe(sass({outputStyle: 'compressed'}))
+		.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+    .pipe(autoprefixer( 'last 2 version', 'ie 8', 'ie 9' ) )
+    .pipe(notify({title: '💩', icon: '', message: '<%= file.relative %> complete' }))
     .pipe(gulp.dest('../'));
 });
 
 
 
+
+
 /*
- * Watch task
+ * Watch files for changes
+ * Type "gulp watch" to run this function
 */
-gulp.task('default', ['sass'], function() {
-        gulp.watch('sass/**/*.scss', ['sass']);
+gulp.task('watch', function() {
+	gulp.watch('sass/**/*.scss', ['styles']);
+	gulp.watch('js/**/*.js', ['scripts']);
 });
+
+
+
+
+
+/*
+ * Default task's that run when you type "gulp" in terminal
+*/
+gulp.task('default', ['styles', 'scripts']);
