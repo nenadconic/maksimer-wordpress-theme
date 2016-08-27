c78b7bef08860b0a5573b325ea79f4155b3cbe63
Kim Bertelsen <kim@maksimer.no>
2016-08-27 20:56:09 +0200

    Removed some spacing

--- page.php
+++ page.php
@@ -2,11 +2,9 @@
 <?php get_header(); ?>
 
 	<section class="main-content-wrap">
-
 		<article id="post-id-<?php the_id(); ?>" <?php post_class( 'clearfix' ); ?>>
 			<?php the_content(); ?>
 		</article>
-
 	</section> <?php // .main-content-wrap ?>
 
 	<div class="wrapper">
