f6cdef863e7134c19362387bc7bfe73ce061e008
Kim Bertelsen <kim@maksimer.no>
2016-11-18 12:05:17 +0100

    Changed content type head element

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
 		<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=yes" />
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<?php wp_head(); ?>	
 	</head>
 	<body <?php body_class(); ?>>
