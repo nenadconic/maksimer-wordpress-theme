45f3ff6ca89ebd2542bb10473a38ec4f085ba874
Kim Bertelsen <kim@maksimer.no>
2016-11-18 12:09:21 +0100

    Fixed yoast description

--- search.php
+++ search.php
@@ -20,14 +20,14 @@
 
 					<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
 
-					<?php
-						$yoast_desc = get_post_meta( $post->ID, '_yoast_wpseo_metadesc' );
-						if ( ! empty( $yoast_desc ) ) {
-							echo '<p>' . $yoast_desc . '</p>';
-						} else {
-							the_excerpt();
-						}
-					?>
+						<?php
+							$yoast_desc = get_post_meta( $post->ID, '_yoast_wpseo_metadesc' );
+							if ( ! empty( $yoast_desc ) ) {
+								echo '<p>' . $yoast_desc[0] . '</p>';
+							} else {
+								the_excerpt();
+							}
+						?>
 
 				</article>
 
