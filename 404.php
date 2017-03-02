e88cb960de0eecbdee568c356fe72b967ed8e52c
Kim Bertelsen <kim@maksimer.no>
2017-03-02 11:55:01 +0100

    Removed comment

--- 404.php
+++ 404.php
@@ -7,12 +7,10 @@
 			<article class="content-404">
 				<h1><?php _e( '404 - Page not found', 'maksimer_lang' ); ?></h1>
 				<h2><?php _e( 'The page you are looking for does not exist', 'maksimer_lang' ); ?>.</h2>
-				<p>
-					<a href="<?php echo home_url(); ?>" class="button"><?php _e( 'Home Page', 'maksimer_lang' ); ?></a>
-				</p>
+				<p><a href="<?php echo home_url(); ?>" class="button"><?php _e( 'Home Page', 'maksimer_lang' ); ?></a></p>
 			</article>
 
-		</div> <?php // .wrapper ?>
+		</div>
 
 	</main>
 
