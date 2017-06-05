d8b83f6a0dfd50f18181d0ad7a105c984e4b302b
Kim Bertelsen <kim@maksimer.no>
2017-06-05 11:27:44 +0200

    Consistant line breaks and no closing comments

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 		<footer class="footer">
 
 			<div class="wrapper">
-				<?php echo date( 'Y' ); ?> <a href="http://www.maksimer.no/" target="_blank">Maksimer</a>
+				<?php echo date( 'Y' ); ?> <a href="http://www.maksimer.no/" target="_blank"><?php __( 'Maksimer', 'maksimer_lang' ); ?></a>
 			</div>
 
 		</footer>
