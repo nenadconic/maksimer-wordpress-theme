04a4acd9d42428c13a0455359800ae4522bbae5e
Kim Bertelsen <kim@maksimer.no>
2018-02-19 08:08:34 +0100

    Corrected line endings. Coding standards

--- header.php
+++ header.php
@@ -4,11 +4,11 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
 		<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=yes" />
-		<?php wp_head(); ?>	
+		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
-		<a class="skip-link screen-reader-text" href="#main-menu-wrapper"><?php _e( 'Skip to navigation', 'maksimer_lang' ); ?></a>
-		<a class="skip-link screen-reader-text" href="#main-content"><?php _e( 'Skip to content', 'maksimer_lang' ); ?></a>
+		<a class="skip-link screen-reader-text" href="#main-menu-wrapper"><?php esc_attr_e( 'Skip to navigation', 'maksimer_lang' ); ?></a>
+		<a class="skip-link screen-reader-text" href="#main-content"><?php esc_attr_e( 'Skip to content', 'maksimer_lang' ); ?></a>
 
 		<header class="header" role="banner">
 
@@ -16,18 +16,26 @@
 
 				<div class="logo">
 					<?php
-						if ( has_custom_logo() ) {
-							the_custom_logo();
-						} else {
-							echo '<a href="' . get_home_url() . '">';
-								bloginfo( 'name' );
-							echo '</a>';
-						}
+					if ( has_custom_logo() ) {
+						the_custom_logo();
+					} else {
+						echo '<a href="' . esc_url( get_home_url() ) . '">';
+							bloginfo( 'name' );
+						echo '</a>';
+					}
 					?>
 				</div>
 
 				<nav class="main-menu" id="main-menu-wrapper" role="navigation">
-					<?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'container' => false, 'fallback_cb' => false ) ); ?>
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'main-menu',
+							'container'      => false,
+							'fallback_cb'    => false,
+						)
+					);
+					?>
 				</nav>
 
 			</div>
