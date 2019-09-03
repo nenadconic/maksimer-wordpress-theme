6ca100903f1a733e16be534dfe2053af096636f4
Kristoffer Eide <kristoffer@maksimer.no>
2019-09-03 17:15:53 +0200

    test code

--- archive.php
+++ archive.php
@@ -15,6 +15,8 @@ get_header();
 			<?php
 			if ( have_posts() ) {
 
+				mb_stristr();
+
 				the_archive_title( '<h1>', '</h1>' );
 				the_archive_description();
 
--- assets/js/maksimer.js
+++ assets/js/maksimer.js
@@ -2,3 +2,6 @@
  * Document ready
  */
 jQuery( () => {} );
+
+fetch();
+
