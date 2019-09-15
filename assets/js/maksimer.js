f52711c860fd06bd9a2f088c77bc119097e133c2
Kim Bertelsen <kim@maksimer.no>
2019-09-15 13:52:56 +0200

    Added some test js data to check for changes

--- assets/js/maksimer.js
+++ assets/js/maksimer.js
@@ -1,4 +1,27 @@
 /**
  * Document ready
  */
-jQuery( () => {} );
+jQuery( () => { } );
+
+const first = 'Kim';
+
+const name = `Your name is ${ first }.`;
+
+const fourAgreements = `You have the right to be you.
+You can only be you when you do your best.`;
+
+const evens = [ 1, 2, 3, 4, 5 ];
+
+const spread = [ ...evens ];
+
+const odds = evens.map( ( n ) => n + 1 );
+
+$( '.btn' ).click( ( event ) => {
+	this.sendData();
+} );
+
+const wait1000 = new Promise( ( resolve, reject ) => {
+	setTimeout( resolve, 1000 );
+} ).then( () => {
+	console.log( 'Yay!' );
+} );
