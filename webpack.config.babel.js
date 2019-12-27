98f8a13b2a872a2da4506a762cd86c887cd1b411
Kristoffer Eide <kristoffer@maksimer.no>
2019-12-27 15:18:12 +0100

    Adds imagemin to webpack

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -38,12 +38,45 @@ const config = {
 						babelrc: true,
 					}
 				}]
-			}
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				use: [
+					{
+						loader: 'file-loader', // Or `url-loader` or your other loader
+					},
+				],
+			},
 		]
 	},
 	mode: process.env.NODE_ENV,
 	plugins: [
 		new webpack.NoEmitOnErrorsPlugin(),
+		new ImageminPlugin({
+			bail: false, // Ignore errors on corrupted images
+			cache: true,
+			imageminOptions: {
+				// Before using imagemin plugins make sure you have added them in `package.json` (`devDependencies`) and installed them
+
+				// Lossless optimization with custom option
+				// Feel free to experiment with options for better result for you
+				plugins: [
+					['gifsicle', {interlaced: true}],
+					['jpegtran', {progressive: true}],
+					['optipng', {optimizationLevel: 5}],
+					[
+						'svgo',
+						{
+							plugins: [
+								{removeViewBox: false},
+								{mergePaths: false},
+								{cleanupIDs: false},
+							],
+						},
+					],
+				],
+			},
+		}),
 	],
 	stats: {
 		colors: true
