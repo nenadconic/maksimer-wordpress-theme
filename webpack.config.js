04110edf982965c9e84e47766f5f4b9ccc02ae37
Kristoffer Eide <kristoffer@maksimer.no>
2019-12-30 15:12:18 +0100

    Proper setup of imagemin in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,24 @@
-const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
-const path = require( 'path' );
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = {
 	...defaultConfig,
 	entry: {
-		'./build/js/index': path.resolve( process.cwd(), 'assets/js', 'index.js' ),
-		'./build/css/sass': path.resolve( process.cwd(), 'assets/sass', 'admin.scss' ),
+		'./build/js/maksimer': path.resolve(process.cwd(), 'assets/js',
+				'maksimer.js'),
+		'./build/js/test': path.resolve(process.cwd(), 'assets/js',
+				'test.js'),
+		//'./build/css/sass': path.resolve( process.cwd(), 'assets/sass', 'admin.scss' ),
 		// 'editor-style': path.resolve( process.cwd(), 'assets/sass', 'editor-style.scss' ),
 		// index: path.resolve( process.cwd(), 'assets/sass', 'index.scss' ),
 	},
 
 	output: {
 		filename: '[name].js',
-		path: path.resolve( process.cwd() ),
+		path: path.resolve(process.cwd()),
 	},
 
 	module: {
@@ -27,41 +32,48 @@ module.exports = {
 					{
 						loader: 'css-loader',
 						options: {
-							minimize: ( 'production' === process.env.NODE_ENV ),
+							minimize: ('production' === process.env.NODE_ENV),
 						},
 					},
 					'postcss-loader',
-					{ loader: 'sass-loader' },
+					{loader: 'sass-loader'},
+				],
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				use: [
+					{
+						loader: 'file-loader', // Or `url-loader` or your other loader
+					},
 				],
 			},
 		],
 	},
 	plugins: [
-		new webpack.NoEmitOnErrorsPlugin(),
+		// Copy the images folder and optimize all the images
+		new CopyWebpackPlugin([
+			{
+				from: 'assets/images', to: 'build/images',
+			}]),
 		new ImageminPlugin({
-			bail: false, // Ignore errors on corrupted images
-			cache: true,
-			imageminOptions: {
-				// Before using imagemin plugins make sure you have added them in `package.json` (`devDependencies`) and installed them
-
-				// Lossless optimization with custom option
-				// Feel free to experiment with options for better result for you
-				plugins: [
-					['gifsicle', {interlaced: true}],
-					['jpegtran', {progressive: true}],
-					['optipng', {optimizationLevel: 5}],
-					[
-						'svgo',
-						{
-							plugins: [
-								{removeViewBox: false},
-								{mergePaths: false},
-								{cleanupIDs: false},
-							],
-						},
-					],
+			test: /\.(jpe?g|png|gif|svg)$/i,
+			plugins: [
+				['gifsicle', {interlaced: true}],
+				['jpegtran', {progressive: true}],
+				['optipng', {optimizationLevel: 5}],
+				[
+					'svgo',
+					{
+						plugins: [
+							{
+								removeViewBox: false,
+								mergePaths: false,
+								cleanupIDs: false,
+							},
+						],
+					},
 				],
-			},
+			],
 		}),
-	]
+	],
 };
